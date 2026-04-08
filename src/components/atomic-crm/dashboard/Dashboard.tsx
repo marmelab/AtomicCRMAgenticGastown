891dbc922025fafb33a7e42ba2eb3fe248da7f73
mayor <jerome@marmelab.com>
2026-04-08 15:38:34 +0200

    feat: add revenue widget to dashboard (CA month/year split by type)

--- src/components/atomic-crm/dashboard/Dashboard.tsx
+++ src/components/atomic-crm/dashboard/Dashboard.tsx
@@ -5,6 +5,7 @@ import { DashboardActivityLog } from "./DashboardActivityLog";
 import { DashboardStepper } from "./DashboardStepper";
 import { DealsChart } from "./DealsChart";
 import { HotContacts } from "./HotContacts";
+import { RevenueWidget } from "./RevenueWidget";
 import { TasksList } from "./TasksList";
 import { Welcome } from "./Welcome";
 
@@ -47,6 +48,7 @@ export const Dashboard = () => {
     <div className="grid grid-cols-1 md:grid-cols-12 gap-6 mt-1">
       <div className="md:col-span-3">
         <div className="flex flex-col gap-4">
+          <RevenueWidget />
           {import.meta.env.VITE_IS_DEMO === "true" ? <Welcome /> : null}
           <HotContacts />
         </div>
